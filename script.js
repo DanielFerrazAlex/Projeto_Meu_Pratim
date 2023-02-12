79f4c185fd611f9d40228820b521ceeb05e3ec1d
Mikael <mikael.previtera@gmail.com>
2023-02-12 13:32:12 -0300

    third commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const mostrarEscolha = document.getElementById('mostrarEscolha');
+
+mostrarEscolha.onclick = function (){
+  const acompanhamento = document.getElementById('acompanhamento').elements;
+  const tamanhoPrato = document.getElementById('tamanhoPrato').elements;
+  const mistura = document.getElementById('mistura').elements;
+  
+  let resultaddo = ``;
+  for(let i = 0; i < 3; i++){
+    if(acompanhamento[i].checked){
+      resultaddo = resultaddo + acompanhamento[i].value + " "
+    }
+    if(tamanhoPrato[i].checked){
+      resultaddo = resultaddo + tamanhoPrato[i].value + " "
+    }
+    if(mistura[i].checked){
+      resultaddo = resultaddo + mistura[i].value + " "
+    }
+  }
+  document.getElementById("escolhas").innerHTML  = resultaddo
+  console.log(resultaddo);
+}
+
